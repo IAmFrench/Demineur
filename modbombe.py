e49b1ddd8501a1f55a9f0235242c1c3d8762f310
raflamaiche <raflamaiche@gmail.com>
2015-04-02 09:26:36 +0200

    Update mars Raphael

--- modbombe.py
+++ modbombe.py
@@ -1,1 +1,19 @@
 from prop import *
+import random
+from modcases import *
+from modgrille import*
+
+cases(xgrille,ygrille)
+
+def bombplace():
+    
+    nbbombe=int(input("Entrez le nombre de bombe: "))
+    for x in range(nbbombe):
+        al=str(random.randint(1,xgrille))+'x'+str(random.randint(1,ygrille))
+        while gr(al,'bombe','statut')==True:
+            al=str(random.randint(1,xgrille))+'x'+str(random.randint(1,ygrille))
+        gr(al,'bombe',True)
+    
+#bombplace()
+#print(grille)    
+
