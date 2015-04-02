4e102f82d01304d67da61cc8eaef45381cd19dc8
alexstar7 <alexandre.quintais@gmail.com>
2015-04-02 10:00:53 +0200

    Update modgraph.py

--- modgraph.py
+++ modgraph.py
@@ -1,3 +1,11 @@
 #Module graphique
-alexandre
-Alexandre
+
+from tkinter import *
+fenetre=Tk()
+fenetre.geometry("1500x1500")
+zone_dessin=Canvas(fenetre,width=1000,height=1000,bg='white')
+zone_dessin.place(x=10,y=20)
+for i in range() :
+    
+fenetre.mainloop()
+
