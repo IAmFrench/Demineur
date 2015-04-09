e9df6c4bdb67918c05abeb1654561c175b047d07
IAmFrench <IAmFrench@users.noreply.github.com>
2015-04-09 09:29:13 +0200

    re-modification

--- modcases.py
+++ modcases.py
@@ -1,26 +1,13 @@
 """
 Module Cases
-Objectif : créer les différentes
-cases en fonction des propriétés
-de la grille (taille)
 Usage dictionnaire :
 http://openclassrooms.com/courses/apprenez-a-programmer-en-python/les-dictionnaires-2
-
+Explication détaillé de la fonction :
+https://github.com/IAmFrench/Demineur/wiki/Fonction-modcases
 """
 from modgrille import * #besoin du dico grille+tailles
 from prop import *
-"""
-documentation :
-grille {'1x1' : [1,2,5,7]
-0-1=bombe ou non
-2-3=drapeau ou non
-4-5=? ou non
-6-7=dévoilé ou non
-dans ce cas ci, la case
-1x1 n'as pas de bombe, a un drapeau, 
-pas de ? et n'est pas dévoilé.
-case[4]=0 -> nombre de cases à definir
-"""
+
 def cases(xgrille,ygrille):
     """ Fonction qui crée de la grille """
     for x in range(1,xgrille+1) :
@@ -28,16 +15,6 @@ def cases(xgrille,ygrille):
             ncase=str(x)+'x'+str(y) #entièrement inutile, juste pour ahérer le code
             grille[ncase]=[1,3,5,7,0]#ni bombe,ni drapeau, ni ? et ni dévoilée
 
-
-"""
-normalement à ce stade la grille est pleine de cases
-resultat du dictionnaire grille :
-
-grille={'1x1' = [1,3,5,7],
-        '1x2' = [1,3,5,7],
-         ...
-        }
-"""
 
 def gr(xygrille,prop,statut):
     """
@@ -60,7 +37,7 @@ def gr(xygrille,prop,statut):
         true
         False
         Statut
-        ou chiffre pour chiffre
+        (ou chiffre pour chiffre)
     """
         
     definition=grille[xygrille] #charge les propriété de la clé demandé
@@ -143,24 +120,22 @@ def gr(xygrille,prop,statut):
                     if a==6:
                         propriete[3]=7
         grille[xygrille]=propriete#met à jours la définition de la clef
-    if statut != True and statut != False and statut==int and prop=='chiffre' : #satut == un entier
-        propriete[4]=int(statut)
-        
-        
+    """
+    Spécial pour le nombre de bombes
+    """
+    if statut != bool and type(statut)==int and prop=='chiffre' : #satut == un entier différent de booleen
+        definition[4]=int(statut)
 """
 Usage de la fonction gr()
-
 exemple 1 :
         gr('1x1','drapeau',True)
     résultat :
         la case de coordonnée 1par1 a un drapeau, donc 3 passe à 2
-
 exemple2:
         (on admet que la case 1x1 a une bombe)
         gr('1x1','bombe','statut')
     résultat :
         retourne la valeur True
-
 exemple3:
         gr('1x1')
         
