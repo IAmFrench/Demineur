7974d2486f25cc60afe2181aeaca170a998f9646
raflamaiche <raflamaiche@users.noreply.github.com>
2015-04-09 10:04:56 +0200

    Update modbombe.py

--- modbombe.py
+++ modbombe.py
@@ -17,5 +17,19 @@ def bombplace():
 #bombplace()
 #print(grille)    
 
+def num():
+    if gr(grillereg,'bombe','statut')==True:
+        gr(xyg,'chiffre',(gr(xyg,'chiffre',statut)+1))
+        
+        
+
 def bombchiffre():
+    for i in range(0;int(xgrille*ygrille)): 
+        
+        xyg=str(x)+x+str(y)
+        grillereg=str(xgrille-1)+x+str(ygrille)
+        if grillereg!=0:
+           num()
+        
+        grillereg=str(xgrille+1)+x+str(ygrille)
     
