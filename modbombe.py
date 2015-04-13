2b34f0a734da3b4669a092ecce56ec5a125862ee
raflamaiche <raflamaiche@users.noreply.github.com>
2015-04-13 18:40:34 +0200

    chiffre modbombe

--- modbombe.py
+++ modbombe.py
@@ -10,26 +10,71 @@ def bombplace():
     nbbombe=int(input("Entrez le nombre de bombe: "))
     for x in range(nbbombe):
         al=str(random.randint(1,xgrille))+'x'+str(random.randint(1,ygrille))
-        while gr(al,'bombe','statut')==True:
+        while gr(al,'bombe','statut')==False:
             al=str(random.randint(1,xgrille))+'x'+str(random.randint(1,ygrille))
         gr(al,'bombe',True)
     
-#bombplace()
-#print(grille)    
+
+    
 
 def num():
     if gr(grillereg,'bombe','statut')==True:
         gr(xyg,'chiffre',(gr(xyg,'chiffre',statut)+1))
-        
+   #return(grille)     
         
 
 def bombchiffre():
-    for i in range(0;int(xgrille*ygrille)): 
+    for x in range(0,int(xgrille)):
+        for y in range(0,int(ygrille)):
+            xyg=str(x)+"x"+str(y)
+            
+            grillereg=str(x-1)+"x"+str(y)
+            if x-1!=0:
+                num()
         
-        xyg=str(x)+x+str(y)
-        grillereg=str(xgrille-1)+x+str(ygrille)
-        if grillereg!=0:
-           num()
+            grillereg=str(x+1)+"x"+str(y)
+            if x+1!=(int(xgrille)+1):
+                num()
+                
+            grillereg=str(x)+"x"+str(y+1)
+            if y+1!=(int(xgrille)+1):
+                num()
+            
+            grillereg=str(x)+"x"+str(y-1)
+            if y-1!=0:
+                num()
+            
+            grillereg=str(x-1)+"x"+str(y-1)
+            if y-1!=0 and x-1!=0:
+                num()
+            
+            grillereg=str(x+1)+"x"+str(y+1)
+            if y+1!=(int(ygrille)+1) and x+1!=(int(xgrille)+1):
+                num()
+            
+            grillereg=str(x+1)+"x"+str(y-1)
+            if y-1!=0 and x+1!=(int(xgrille)+1):
+                num()
+                
+            grillereg=str(x-1)+"x"+str(y+1)
+            if y+1!=(int(ygrille)+1) and x-1!=0:
+                num()
         
-        grillereg=str(xgrille+1)+x+str(ygrille)
-    
+    #return(grille)
+
+
+            
+cases(xgrille,ygrille)
+print("adc")
+bombplace()
+print(grille)
+bombchiffre() 
+
+
+
+
+
+
+
+
+
