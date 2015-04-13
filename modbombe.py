6d3249c4241520302adceaccb9dd49c64c8c2f5f
raflamaiche <raflamaiche@users.noreply.github.com>
2015-04-13 18:58:03 +0200

    correction bug 2
    fkpo

--- modbombe.py
+++ modbombe.py
@@ -17,48 +17,47 @@ def bombplace():
 
     
 
-def num(grillereg):
+def num(grillereg,xyg):
     if gr(grillereg,'bombe','statut')==True:
-        gr(xyg,'chiffre',(gr(xyg,'chiffre',statut)+1))
-   #return(grille)     
+        gr(xyg,'chiffre',int(gr(xyg,'chiffre','statut')+1))
         
 
 def bombchiffre():
-    for x in range(0,int(xgrille)):
-        for y in range(0,int(ygrille)):
+    for x in range(1,int(xgrille)):
+        for y in range(1,int(ygrille)):
             xyg=str(x)+"x"+str(y)
             
             grillereg=str(x-1)+"x"+str(y)
             if x-1!=0:
-                num(grillereg)
+                num(grillereg,xyg)
         
             grillereg=str(x+1)+"x"+str(y)
             if x+1!=(int(xgrille)+1):
-                num(grillereg)
+                num(grillereg,xyg)
                 
             grillereg=str(x)+"x"+str(y+1)
             if y+1!=(int(xgrille)+1):
-                num(grillereg)
+                num(grillereg,xyg)
             
             grillereg=str(x)+"x"+str(y-1)
             if y-1!=0:
-                num(grillereg)
+                num(grillereg,xyg)
             
             grillereg=str(x-1)+"x"+str(y-1)
             if y-1!=0 and x-1!=0:
-                num(grillereg)
+                num(grillereg,xyg)
             
             grillereg=str(x+1)+"x"+str(y+1)
             if y+1!=(int(ygrille)+1) and x+1!=(int(xgrille)+1):
-                num(grillereg)
+                num(grillereg,xyg)
             
             grillereg=str(x+1)+"x"+str(y-1)
             if y-1!=0 and x+1!=(int(xgrille)+1):
-                num(grillereg)
+                num(grillereg,xyg)
                 
             grillereg=str(x-1)+"x"+str(y+1)
             if y+1!=(int(ygrille)+1) and x-1!=0:
-                num(grillereg)
+                num(grillereg,xyg)
     
         
     #return(grille)
@@ -66,8 +65,8 @@ def bombchiffre():
 
             
 cases(xgrille,ygrille)
-print("adc")
+
 bombplace()
-print(grille)
+
 bombchiffre() 
 
