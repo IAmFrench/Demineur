3193146ff672d4a11a54a81dadae3ca0d83b49fd
IAmFrench <IAmFrench@users.noreply.github.com>
2015-05-05 16:31:47 +0200

    Grande MAJ fonction GR()
    #Restructuration de la fonction gr
    >Pas encore au point
    >ébauche

--- modcases.py
+++ modcases.py
@@ -15,7 +15,212 @@ def cases(xgrille,ygrille):
             ncase=str(x)+'x'+str(y) #entièrement inutile, juste pour ahérer le code
             grille[ncase]=[1,3,5,7,0]#ni bombe,ni drapeau, ni ? et ni dévoilée
 
+"""
+>>
+    Rappels
+>>
+"""
+"""
+liste des diff. prop :
+    -bombe
+    -drapeau
+    -interrogation
+    -visible
+    -chiffre
+Rappel (o=oui/n=non):
+    obombe=0
+    nbombe=1
+    odrapeau=2
+    ndrapeau=3
+    ointerro=4
+    ninterro=5
+    ovisible=6
+    nvisible=7
+Statuts :
+    true
+    False
+    Statut
+    (ou chiffre pour chiffre)
+"""
 
+
+
+"""
+>>
+    Fonction gr
+>>
+"""
+def gr(xygrille,prop,statut):
+    definition=grille[xygrille] #charge les propriété de la clé demandé
+    ###################
+    ###Les variables###
+    ###################
+    
+    
+    ###################
+    ###Les Fonctions###
+    ###################
+    """
+    >>
+        Affiche le statut d'une case
+    >>
+    """
+    def case_statut():
+        """ Affiche le statut (True/False) de la case """
+        if prop=='bombe':
+            if 0 in definition:
+                 rbombe=True
+                 return(rbombe)
+            else :
+                rbombe=False
+                return(rbombe)
+                
+        elif prop=='drapeau':
+            if 2 in definition:
+                rdrapeau=True
+                return(rdrapeau)
+            else :
+                rdrapeau=False
+                return(rdrapeau)
+                
+        elif prop=='interrogation':
+            if 4 in definition:
+                rinterro=True
+                return(rinterro)
+            else:
+                rinterro=False
+                return(rinterro)
+                
+        elif prop=='visible':
+            if 6 in definition:
+                rvisible=True
+                return(rvisible)
+            else:
+                rvisible=False
+                return(rvisible)
+        elif prop=='chiffre':
+            return int((definition[4])) #ENTIER
+    
+    
+    """
+    >>
+        Modifie le statut d'une case (non->oui)
+    >>
+    """
+    def case_offon():
+        """ Active la propriété """
+        for a in definition :
+            """
+            Passe de non vers oui
+            """
+            if statut==True:
+                propriete=definition
+                if prop=='bombe':
+                    if a==1:
+                        propriete[0]=0
+                elif prop=='drapeau':
+                    if a==3:
+                        propriete[1]=2
+                elif prop=='interrogation':
+                    if a==5:
+                        propriete[2]=4
+                elif prop=='visible':
+                    if a==7:
+                        propriete[3]=6
+        grille[xygrille]=propriete #met à jours la définition de la clef
+
+    
+    """
+    >>
+        Modifie le statut d'une case (oui->non)
+    >>
+    """
+    def case_onoff():
+        """ Désactive le statut de la propriété """
+        """
+            Passe de oui vers non
+        """
+        for a in definition :
+            if statut==False:
+                propriete=definition
+                if prop=='bombe':
+                    if a==0:
+                        propriete[0]=1
+                elif prop=='drapeau':
+                    if a==2:
+                        propriete[1]=3
+                elif prop=='interrogation':
+                    if a==4:
+                        propriete[2]=5
+                elif prop=='visible':
+                    if a==6:
+                        propriete[3]=7
+        grille[xygrille]=propriete #met à jours la définition de la clef
+        
+    def case_nbbombe():
+        """ Renvoi la valeur du nombre de bombe """
+        """
+        Spécial pour le nombre de bombes
+        """
+        if statut != bool and type(statut)==int and prop=='chiffre' : #satut == un entier différent de booleen
+            definition[4]=int(statut)
+        
+    ##################
+    ###Test logique###
+    ##################
+    
+    if statut=='statut':
+        return(case_statut())
+        ###
+        print("Statut='statut'")
+        ###
+        
+    if statut==True: #On demande a activer la propriété
+        return(case_offon())
+        ###
+        print("statut=True")
+        ###
+    
+    if statut==False: #On demande a désactiver la propriété
+        return(case_onoff())
+        ###
+        print("Statut=False")
+        ###
+    
+
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
 def gr(xygrille,prop,statut):
     """
     liste des diff. prop :
@@ -124,6 +329,9 @@ def gr(xygrille,prop,statut):
     """
     if statut != bool and type(statut)==int and prop=='chiffre' : #satut == un entier différent de booleen
         definition[4]=int(statut)
+        
+'''
+
 """
 Usage de la fonction gr()
 exemple 1 :
@@ -139,3 +347,7 @@ exemple3:
         gr('1x1')
         
 """
+
+print(gr('1x1','bombe','statut'))
+print(gr('1x2','bombe',True))
+
