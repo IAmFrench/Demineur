a8f450823c83c60135d49bc4586cba760f75153e
IAmFrench <IAmFrench@users.noreply.github.com>
2015-05-07 10:02:30 +0200

    Update prop.py

--- prop.py
+++ prop.py
@@ -1,1 +1,15 @@
-
+def difficulte('niveau'):
+    """
+    3 niveaux de difficulté
+        "facile" -> 9x9
+        "intermediaire" -> 16x16
+        "expert" -> 30x16
+    """
+    if niveau=='facile':
+        return('9x9')
+    elif niveau=='intermediaire':
+        return('16x16')
+    elif niveau=='expert':
+        return('30x16')
+    else :
+        return('Erreur')
