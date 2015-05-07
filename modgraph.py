2d861c4fea1b202d1b1b23c6139e0c0745a3ef2e
alexstar7 <alexandre.quintais@gmail.com>
2015-05-07 09:22:15 +0200

    Update modgraph.py

--- modgraph.py
+++ modgraph.py
@@ -1,14 +1,24 @@
 #Module graphique
 
 from tkinter import *
+from core import *
+from modegrille import *
 fenetre=Tk()
-fenetre.geometry("1500x1500")
-zone_dessin=Canvas(fenetre,width=1000,height=1000,bg='white')
-xgrille=8
-ygrille=10
+xgrille=str(input("que voulez vous?"))
+ygrille=str(input("que voulez vous?"))
+xgrille=int(xgrille)
+ygrille=int(ygrille)
+taille=str((xgrille*50+20))+"x"+str((ygrille*50+50))
+fenetre.geometry(taille)
+zone_dessin=Canvas(fenetre,width=550,height=1000,bg='white')
 zone_dessin.place(x=10,y=20)
-for i in range (0,xgrille):
-    
-    zone_dessin.create_line(20+50*i,50,20+50*i,50+50*(ygrille+1))
-    zone_dessin.create_line(20,50+50*i,50+50*(xgrille+1),50+50*i)
-fenetre.mainloop()
+xgrille=10
+ygrille=10
+for i in range (0,xgrille+1):    
+    zone_dessin.create_line(20+50*i,50,20+50*i,50+50*(ygrille))
+ 
+for i in range(0,ygrille+1):
+    zone_dessin.create_line(20,50+50*i,20+50*(xgrille),50+50*i)
+  
+fenetre.mainloop() 
+
